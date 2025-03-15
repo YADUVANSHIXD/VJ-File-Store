8141f92b77bfc780b3ba9c07d38b66efd95b80d1
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2025-03-15 20:02:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,11 +9,11 @@ class script(object):
 
 
     
-    CAPTION = """<b>📂 {file_caption}
+    CAPTION = """<b>📂 {file_name}
 
 ⚙️ sɪᴢᴇ : {file_size}
 
-✯ 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/+-JNC-lsTB5IxZjg1">🍿𝗨𝗽𝗱𝗮𝘁𝗲 𝗰𝗵𝗮𝗻𝗻𝗲𝗹🍿</a>」 
+✯𝖩𝗈𝗂𝗇➥<a href="https://t.me/+-JNC-lsTB5IxZjg1">🍿𝗨𝗽𝗱𝗮𝘁𝗲 𝗰𝗵𝗮𝗻𝗻𝗲𝗹🍿</a> 
 •─────────★•🥰•★────────•</b>""" 
 
        
